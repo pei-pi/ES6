b27fec153a233348086b613de95be1d6c76b712c
peipi <2390574106@qq.com>
2023-12-13 10:43:13 +0800

    g

---
+++ Generator/generator7.js
@@ -0,0 +1,13 @@
+const Thunk = function(fn) {
+    return function (...args) {
+      return function (callback) {
+        return fn.call(this, ...args, callback);
+      }
+    };
+  };
+  function f(a, cb) {
+    cb(a);
+  }
+  const ft = Thunk(f);
+  
+  ft(1)(console.log) // 1
